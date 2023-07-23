796a49e5962547cbf33b47ed8adebe1390ea8053
Khkimov <khakim198765@gmail.com>
2023-07-23 17:28:53 +0500

    typing ref

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,32 @@
 <script setup lang="ts">
+import { reactive } from "vue";
 import TheHeader from "@/components/TheHeader.vue";
 import EntryEditor from "./components/EntryEditor.vue";
 import EntryCard from "@/components/EntryCard.vue";
+import type User from "@/types/User";
+import type Entry from "@/types/Entry";
+
+const entries: Entry[] = reactive([]);
+
+const user: User = reactive({
+  id: 1,
+  userName: "Ilyas",
+  settings: [],
+});
+console.log(user.id);
+
+const handleCreateEntry = (entry: Entry) => {
+  entries.unshift(entry);
+};
 </script>
 
 <template>
   <main class="container m-auto p-10">
     <TheHeader />
-    <EntryEditor />
+    <EntryEditor @@create="handleCreateEntry" />
     <ul>
-      <li>
-        <EntryCard />
+      <li v-for="entry in entries" :key="entry.id">
+        <EntryCard :entry="entry" />
       </li>
     </ul>
   </main>
--- src/components/EntryEditor.vue
+++ src/components/EntryEditor.vue
@@ -1,13 +1,56 @@
 <script lang="ts" setup>
+import { computed, onMounted, ref } from "vue";
 import EmojiField from "@/components/EmojiField.vue";
 import ArrowCircleRight from "@/assets/icons/arrow-circle-right.svg";
+import type Emoji from "@/types/Emoji";
+import type Entry from "@/types/Entry";
+
+const body = ref("");
+const emoji = ref<Emoji | null>(null);
+const charCount = computed(() => body.value.length);
+const maxChars = 280;
+
+const textarea = ref<HTMLTextAreaElement | null>(null)
+
+onMounted(() => textarea.value?.focus())
+
+const emit = defineEmits<{
+  (e: "@create", entry: Entry): void;
+}>();
+
+const handleTextInput = (e: Event) => {
+  const textarea = e.target as HTMLTextAreaElement;
+  if (textarea.value.length <= maxChars) {
+    body.value = textarea.value;
+  } else {
+    body.value = textarea.value = textarea.value.substring(0, maxChars);
+  }
+};
+
+const handleSubmit = () => {
+  emit("@create", {
+    body: body.value,
+    emoji: emoji.value,
+    createdAt: new Date(),
+    userId: 1,
+    id: Math.random(),
+  });
+
+  body.value = "";
+  emoji.value = null;
+};
 </script>
 <template>
-  <form class="entry-form" @submit.prevent>
-    <textarea placeholder="New Journal Entry for danielkelly_io"></textarea>
-    <EmojiField />
+  <form class="entry-form" @submit.prevent="handleSubmit">
+    <textarea
+      :value="body"
+      ref="textarea"
+      @keyup="handleTextInput"
+      placeholder="New Journal Entry for Ilyas"
+    ></textarea>
+    <EmojiField v-model="emoji" />
     <div class="entry-form-footer">
-      <span>0 / 280</span>
+      <span>{{ charCount }} / {{ maxChars }}</span>
       <button>Remember <ArrowCircleRight width="20" /></button>
     </div>
   </form>
